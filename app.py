4b44763ce9902e9385e1c3303e825d29df4b3cd4
chatperi-2025 <chatperi2@gmail.com>
2025-04-23 10:23:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def index():
     return render_template("index.html", user_input=user_input, bot_response=bot_response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=5000)
